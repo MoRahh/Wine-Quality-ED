a6e900915786e50d465c3842f671337de1e9d431
MoRahh <moodyaburahma@gmail.com>
2023-05-04 13:22:33 +0300

    Added code to train a linear regression model and save it as a file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+import joblib
 
 # Load the dataset
 df = pd.read_csv('data/winequality-red.csv', sep=';')
@@ -42,12 +43,6 @@ df = pd.get_dummies(df, columns=['quality'])  # One-hot encode any categorical v
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Display the preprocessed data and the sizes of the training and testing sets
-print("X_train shape:", X_train.shape)
-print("y_train shape:", y_train.shape)
-print("X_test shape:", X_test.shape)
-print("y_test shape:", y_test.shape)
-
 # Train the linear regression model on the preprocessed training data
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
@@ -60,6 +55,18 @@ r2 = r2_score(y_test, y_pred)
 
 # Display the R-squared score
 print("R-squared score:", r2)
+
+# Save the model to disk
+joblib.dump(regressor, 'models/wine_quality_model.pkl')
+
+# Load the model from disk
+loaded_model = joblib.load('models/wine_quality_model.pkl')
+
+# Make a prediction on new data
+new_data = [[7.2, 0.33, 0.32, 3.0, 0.034, 23.0, 97.0, 0.9908, 3.16, 0.52, 10.0]]
+prediction = loaded_model.predict(new_data)
+
+print(prediction)
 
 # Display the plot
 plt.show()
