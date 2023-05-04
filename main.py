9fa21f030f5cbdbad6cceda78ada8b9e6aa978b2
MoRahh <moodyaburahma@gmail.com>
2023-05-04 11:27:25 +0300

    Splitting the data into training and testing sets

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 # Load the dataset
@@ -36,10 +37,22 @@ scaler = StandardScaler()  # Scale the features
 X = scaler.fit_transform(X)
 df = pd.get_dummies(df, columns=['quality'])  # One-hot encode any categorical variables
 
-# Display the preprocessed data
-print(X)
-print(y)
-print(df.head())
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Display the preprocessed data and the sizes of the training and testing sets
+print("X_train shape:", X_train.shape)
+print("y_train shape:", y_train.shape)
+print("X_test shape:", X_test.shape)
+print("y_test shape:", y_test.shape)
+
+# Display the first few rows of the training set
+print("Training set:")
+print(pd.DataFrame(X_train).head())
+
+# Display the first few rows of the testing set
+print("Testing set:")
+print(pd.DataFrame(X_test).head())
 
 # Display the plot
 plt.show()
